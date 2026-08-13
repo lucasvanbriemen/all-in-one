ae448b9cf8642ea3832b570e88c703a3743a52b2
Lucas <contact@lucasvanbriemen.nl>
2026-08-13 21:02:57 +0200

    Change remote URL to https://aio.ltvb.nl.

--- react-native/components/api.js
+++ react-native/components/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://aio.ltvb.nl";
 
 export const api = {
   // A getter so every request reads the *current* CSRF token — Turbo swaps
