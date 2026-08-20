f20cc42ad05da81ba488155bc1a1d92b5cabfdfe
Lucas <contact@lucasvanbriemen.nl>
2026-08-20 19:35:08 +0200

    Render app titles in sidebar with Pressable component to activate selected app..

--- react-native/components/sidebar/SidebarApplication.jsx
+++ react-native/components/sidebar/SidebarApplication.jsx
@@ -11,8 +11,10 @@ export function SidebarApplication({selection, onSelect, currentlyActive, setAct
 
   return (
     <View>
-      <Text style={styles.title}>{app}</Text>
-      {item.map(row => (
+      <Pressable onPress={() => setActiveApp(app)}>
+        {!isMinimized && <Text style={styles.title}>{app}</Text>}
+      </Pressable>
+      {currentlyActive === app && item.map(row => (
         <SidebarRow key={row.path} icon={row.path} title={row.name} isSelected={currentlyActive === app && selection === row.path} onPress={() => {onSelect(row.path); setActiveApp(app)}} useLabels={!isMinimized} />
       ))}
     </View>
