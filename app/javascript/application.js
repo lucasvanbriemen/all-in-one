f5053939701d63b355a0249ee5733a63ba35f919
Lucas <contact@lucasvanbriemen.nl>
2026-08-20 18:20:53 +0200

    Make API only.

--- app/javascript/application.js
+++
@@ -1,7 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
-import theme from "objects/theme"
-
-theme.init()
