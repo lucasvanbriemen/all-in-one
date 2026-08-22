f53c49d8c5ec2164460c901c29c2214d00eb0d1c
Lucas <contact@lucasvanbriemen.nl>
2026-08-22 18:57:09 +0200

    Allow for searching functionality.

--- react-native/scripts/fileserver.mjs
+++ react-native/scripts/fileserver.mjs
@@ -8,6 +8,16 @@ const server = http.createServer(async (request, response) => {
   const relative = searchParams.get('path');
   const projectRoot = searchParams.get('projectRoot');
 
+  if (method == "GET" && pathname === "/search") {
+    const searchTerm = searchParams.get('term');
+    if (!searchTerm) {
+      response.writeHead(400, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ error: 'missing term parameter' }));
+      return;
+    }
+    return searchFiles(response, projectRoot, searchTerm);
+  }
+
   if (method == "PUT" && pathname === "/file") {
     return new Promise((resolve) => {
       let body = '';
@@ -122,5 +132,39 @@ async function getDirectoryContents(absolutePath, response, wantedPath) {
     console.error(`Failed to read directory: ${absolutePath}; error: ${error}`);
     response.writeHead(404, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify({ error: 'not found' }));
+  }
+}
+
+async function searchFiles(response, projectRoot, searchTerm) {
+  try {
+    const results = [];
+    await searchDirectory(projectRoot, searchTerm, results);
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({ results }));
+  } catch (error) {
+    console.error(`Failed to search files in: ${projectRoot}; error: ${error}`);
+    response.writeHead(500, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({ error: 'failed to search files' }));
+  }
+}
+
+async function searchDirectory(directory, searchTerm, results) {
+  const FoldersToIgnore = ['node_modules', '.git', 'build', 'dist', 'out', 'venv', '__pycache__'];
+
+  const entries = await fs.readdir(directory, { withFileTypes: true });
+  for (const entry of entries) {
+
+    let entryName = entry.name;
+    entryName = entryName.toLowerCase();
+    const searchTermLower = searchTerm.toLowerCase();
+
+    const fullPath = path.join(directory, entry.name);
+      if (entry.isDirectory()) {
+        if (!FoldersToIgnore.includes(entry.name)) {
+          await searchDirectory(fullPath, searchTerm, results);
+        }
+      } else if (entryName.includes(searchTermLower)) {
+        results.push(fullPath);
+      }
   }
 }
