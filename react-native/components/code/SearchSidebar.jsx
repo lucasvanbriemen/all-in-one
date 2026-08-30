e83dfe37fb4bb05849783e77bc1638d448c9f728
Lucas <contact@lucasvanbriemen.nl>
2026-08-30 19:27:02 +0200

    Remove unnecessary code.

--- react-native/components/code/SearchSidebar.jsx
+++ react-native/components/code/SearchSidebar.jsx
@@ -2,70 +2,17 @@ import {Pressable, ScrollView, StyleSheet, Text, TextInput, View} from 'react-na
 import {glass, useThemedStyles} from '../theme';
 import {useEffect, useState} from 'react';
 
-import {FileIcon} from '../icons/FileIcon';
-import {FileNode} from './FileNode';
-import {NativeModules} from 'react-native';
-import {fileSystem} from '../fileSystem';
-import {sortFiles} from './sortFiles';
 import {useRef} from 'react';
 
 export function SearchSidebar({currentFile, onOpenFile, onSave, projectRoot, setProjectRoot, openedFiles, setOpenedFiles}) {
   const styles = useThemedStyles(createStyles);
-  const [files, setFiles] = useState([]);
 
   const inputRef = useRef(null);
   const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(() => {
-    async function fetchFiles() {
-      const unsortedFiles = await fileSystem.listFiles(projectRoot, '');
-      const sortedFiles = sortFiles(unsortedFiles.contents ?? []);
-      setFiles(sortedFiles);
-    }
-
-    fetchFiles();
-  }, [projectRoot]);
-
-  function handleFileSelect(file) {
-    if (file.isDirectory) {
-      return openDirectory(file);
-    }
-
-    return onOpenFile(file.fullPath);
-  }
-
-  async function openDirectory(file) {
-    const pathToOpen = file.fullPath;
-
-    const response = await fileSystem.listFiles(projectRoot, pathToOpen);
-    const folderItems = response.contents ?? [];
-
-    file.items = folderItems;
-
-    let updatedFiles = [...files];
-    updatedFiles = updatedFiles.map(f => {
-      if (f.name === file.name) {
-        return file;
-      }
-
-      return f;
-    });
-    setFiles(updatedFiles);
-  }
-
-  async function openFolder() {
-    const path = await NativeModules.FolderPicker.pick();
-
-    if (!path) {
-      return;
-    }
-
-    setProjectRoot(path);
-  }
-
   return (
     <ScrollView style={styles.editor}>
-      <TextInput ref={inputRef} style={styles.input} placeholder="Looking for something?" enableFocusRing={false} value={searchTerm}onChangeText={setSearchTerm} />
+      <TextInput ref={inputRef} style={styles.input} placeholder="Looking for something?" enableFocusRing={false} value={searchTerm} onChangeText={setSearchTerm} />
     </ScrollView>
   );
 }
