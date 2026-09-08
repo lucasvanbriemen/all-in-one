900732aec213c7fae3f108d60293ae060469c5b6
Lucas <contact@lucasvanbriemen.nl>
2026-09-08 18:09:17 +0200

    Clean up file even more.

--- react-native/scripts/fileserver/handlers/search.mjs
+++ react-native/scripts/fileserver/handlers/search.mjs
@@ -2,18 +2,16 @@ import {searchCode} from '../search/codeSearch.mjs';
 import {searchFileNames} from '../search/fileSearch.mjs';
 import {sendJson} from '../http.mjs';
 
-const SEARCHES = new Map([
+const SEARCH_OPTIONS = new Map([
   ['files', {name: 'files', run: searchFileNames}],
   ['code', {name: 'code', run: searchCode}],
 ]);
-
-const DEFAULT_SEARCH = SEARCHES.get('code');
 
 export async function search({response, searchParams}) {
   const projectRoot = searchParams.get('projectRoot');
   const term = searchParams.get('term');
 
-  const searching = SEARCHES.get(searchParams.get('type')) || DEFAULT_SEARCH;
+  const searching = SEARCH_OPTIONS.get(searchParams.get('type'));
 
   sendJson(response, 200, {results: await searching.run(projectRoot, term)});
 }
