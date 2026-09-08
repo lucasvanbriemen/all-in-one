6baf175736fbef454221387a66284713aebed67a
Lucas <contact@lucasvanbriemen.nl>
2026-09-08 12:15:59 +0200

    Remove deprecated music model files and code.

--- app/models/music/playlist.rb
+++
@@ -1,6 +0,0 @@
-module Music
-  class Playlist < ApplicationRecord
-    has_many :playlist_songs, dependent: :destroy
-    has_many :songs, through: :playlist_songs
-  end
-end
--- app/models/music/playlist_song.rb
+++
@@ -1,6 +0,0 @@
-module Music
-  class PlaylistSong < ApplicationRecord
-    belongs_to :playlist
-    belongs_to :song, foreign_key: :song_isrc, inverse_of: :playlist_songs
-  end
-end
