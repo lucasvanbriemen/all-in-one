430380219e0627cc4fc2298785382ddaee33339b
Lucas <contact@lucasvanbriemen.nl>
2026-09-10 23:27:13 +0200

    Add play functionality to MusicPage.

--- react-native/components/music/MusicPage.jsx
+++ react-native/components/music/MusicPage.jsx
@@ -1,5 +1,5 @@
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import React, { useEffect, useState } from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 
 import {Song} from './Song';
 import {api} from '../api';
@@ -9,6 +9,9 @@ export function MusicPage({activeSidebarItem}) {
   const styles = useThemedStyles(createStyles);
 
   const [likedSongs, setLikedSongs] = useState([]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const MP3_URL = "https://music.ltvb.nl/api/get-mp3/DED162500001"
 
   useEffect(() => {
     api.get('/music').then(response => {
@@ -19,6 +22,10 @@ export function MusicPage({activeSidebarItem}) {
   return (
     <View style={styles.content}>
       <Text style={styles.greeting}>Hey</Text>
+
+      <Pressable onPress={() => setIsPlaying(!isPlaying)}>
+        <Text>{isPlaying ? 'Pause' : 'Play'}</Text>
+      </Pressable>
 
       {likedSongs.map((song, index) => (
         <Song key={song.isrc} song={song} isEven={index % 2 === 0} />
