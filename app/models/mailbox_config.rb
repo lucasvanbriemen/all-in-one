6ad42ae32263212d8552346144b901c0d23dcf68
Lucas <contact@lucasvanbriemen.nl>
2026-09-10 14:20:34 +0200

    Add notification argument.

--- app/models/mailbox_config.rb
+++ app/models/mailbox_config.rb
@@ -16,11 +16,13 @@ module MailboxConfig
     {
       path: "home",
       name: "Home",
+      send_notifications: true,
       rules: { exclude_from: %w[work github pathe] }
     },
     {
       path: "work",
       name: "Work",
+      send_notifications: true,
       rules: {
         from: %w[*@webinargeek.com],
         to: %w[*@webinargeek.com]
@@ -29,6 +31,7 @@ module MailboxConfig
     {
       path: "github",
       name: "GitHub",
+      send_notifications: false,
       rules: {
         from: %w[*@github.com *@notifications.github.com],
         sender_name: [ "github GUI" ]
@@ -37,6 +40,7 @@ module MailboxConfig
     {
       path: "pathe",
       name: "Pathe",
+      send_notifications: true,
       rules: { from: %w[*@service.pathe.nl] }
     }
   ].freeze
