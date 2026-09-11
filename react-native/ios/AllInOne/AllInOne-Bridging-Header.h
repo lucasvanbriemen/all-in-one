be9d909559df5f68fd5918322c6f45c1b7955a7c
Lucas <contact@lucasvanbriemen.nl>
2026-09-11 18:59:19 +0200

    Expose React Native's Objective-C API to Swift for the native modules. Mirrors the macOS bridging header.

--- react-native/ios/AllInOne/AllInOne-Bridging-Header.h
+++ react-native/ios/AllInOne/AllInOne-Bridging-Header.h
@@ -1,5 +1,2 @@
-// Exposes React Native's Objective-C API to Swift for the native modules
-// under native/ (AudioPlayer). Mirrors the macOS bridging header.
-
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
