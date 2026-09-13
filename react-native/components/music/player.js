df63d3f1681506913fc0382e6cd37487db6fde42
Lucas <contact@lucasvanbriemen.nl>
2026-09-13 19:32:19 +0200

    Fix logic in playPlaylist and next functions.

--- react-native/components/music/player.js
+++ react-native/components/music/player.js
@@ -16,44 +16,38 @@ export const player = {
     set('music.now-playing', song);
   },
 
-  async playPlaylist(songs, set, get, atIndex = 0) {
-    const songsForQueue = [...songs];
+  async playPlaylist(songs, set, atIndex = 0) {
+    const songToPlay = songs[atIndex];
 
-    const songToPlay = songsForQueue[atIndex]; 
-
-    // Push the songs before the index into previous songs
-    const previousSongs = songsForQueue.slice(0, atIndex);
-    set('music.last-songs', previousSongs);
-
-    songsForQueue.splice(atIndex, 1);
-
-    set('music.queue', songsForQueue);
-    if (songsForQueue.length > 0) {
-      await player.play(songToPlay, set);
-    }
+    set('music.last-songs', songs.slice(0, atIndex));
+    set('music.queue', songs.slice(atIndex + 1));
+    await player.play(songToPlay, set);
   },
 
   async next(set, get) {
-    const queue = get('music.queue');
-    if (queue && queue.length > 0) {
-      const nextSong = queue.shift();
-      set('music.queue', queue);
-      await player.play(nextSong, set);
+    const queue = get('music.queue') || [];
+    if (queue.length === 0) return;
+
+    const currentlyPlaying = get('music.now-playing');
+    const lastSongs = get('music.last-songs') || [];
+    if (currentlyPlaying) {
+      set('music.last-songs', [...lastSongs, currentlyPlaying]);
     }
+
+    set('music.queue', queue.slice(1));
+    await player.play(queue[0], set);
   },
 
   async previous(set, get) {
-    const queue = get('music.queue');
+    const queue = get('music.queue') || [];
     const lastSongs = get('music.last-songs') || [];
-    if (lastSongs.length > 0) {
-      const previousSong = lastSongs.pop();
-      set('music.last-songs', lastSongs);
-      if (queue) {
-        queue.unshift(previousSong);
-        set('music.queue', queue);
-      }
-      await player.play(previousSong, set);
-    }
+    if (lastSongs.length === 0) return;
+
+    const currentlyPlaying = get('music.now-playing');
+    const previousSong = lastSongs[lastSongs.length - 1];
+    set('music.last-songs', lastSongs.slice(0, -1));
+    set('music.queue', currentlyPlaying ? [currentlyPlaying, ...queue] : [...queue]);
+    await player.play(previousSong, set);
   },
 
   async pause() {
@@ -69,29 +63,9 @@ export const player = {
     const sub = emitter.addListener('playbackStateChanged', ({isPlaying, ended}) => {
       set('music.now-playing.is-playing', isPlaying);
 
-      console.log('Playback state changed:', {isPlaying, ended});
-
-      if (!isPlaying || !ended) {
-        return;
+      if (ended) {
+        return player.next(set, get);
       }
-
-      const queue = get('music.queue');
-
-      const justFinished = get('music.now-playing');
-      const lastSongs = get('music.last-songs') || [];
-      lastSongs.push(justFinished);
-      set('music.last-songs', lastSongs);
-
-      if (queue && queue.length > 0) {
-        console.log('Queue before playing next song:', queue);
-        set('music.now-playing', queue[0]);
-
-        queue.shift();
-
-        set('music.queue', queue);
-      }
-
-      player.play(queue[0], set);
     });
     return () => sub.remove();
   },
