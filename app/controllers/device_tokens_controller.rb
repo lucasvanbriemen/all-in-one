3ba2c7ee187d64f257c6c2f6a4a25e59a971b80e
Lucas <contact@lucasvanbriemen.nl>
2026-09-14 21:51:30 +0200

    Improve error handling in create action.

--- app/controllers/device_tokens_controller.rb
+++ app/controllers/device_tokens_controller.rb
@@ -2,8 +2,6 @@ class DeviceTokensController < ApplicationController
   def create
     device_token = DeviceToken.register(token: params.require(:token), platform: params.require(:platform))
     render json: device_token, status: :created
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { error: e.message }, status: :unprocessable_entity
   end
 
   def destroy
