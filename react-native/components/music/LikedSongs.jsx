e2c8a139a577261947122849e8dcdea334d617fc
Lucas <contact@lucasvanbriemen.nl>
2026-09-16 20:24:01 +0200

    Render page.

---
+++ react-native/components/music/LikedSongs.jsx
@@ -0,0 +1,51 @@
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+
+import {NowPlayingBanner} from './NowPlayingBanner';
+import {Song} from './Song';
+import {api} from '../api';
+import {player} from './player';
+import {useAppContext} from '../../context/AppContext';
+import {useThemedStyles} from '../theme';
+
+export function LikedSongs() {
+  const styles = useThemedStyles(createStyles);
+
+  const {set} = useAppContext();
+
+  const [likedSongs, setLikedSongs] = useState([]);
+
+  useEffect(() => {
+    api.get('/music').then(response => {
+      setLikedSongs(response);
+    });
+  }, []);
+
+  return (
+    <View style={styles.content}>
+      <NowPlayingBanner />
+
+      <Pressable onPress={() => player.playPlaylist(likedSongs, set)}>
+        <Text>Play All</Text>
+      </Pressable>
+
+      {likedSongs.map((song, index) => (
+        <Song key={song.isrc} song={song} isEven={index % 2 === 0} onClick={() => player.playPlaylist(likedSongs, set, index)} />
+      ))}
+    </View>
+  );
+}
+
+const createStyles = colors => StyleSheet.create({
+  statsContainer: {
+    flexDirection: 'row',
+    gap: 16,
+    paddingTop: 16,
+  },
+  greeting: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginTop: 16,
+    color: colors.outline
+  },
+});
--- react-native/components/music/MusicPage.jsx
+++ react-native/components/music/MusicPage.jsx
@@ -1,51 +1,27 @@
 import {Pressable, StyleSheet, Text, View} from 'react-native';
-import React, {useEffect, useState} from 'react';
 
-import {NowPlayingBanner} from './NowPlayingBanner';
-import {Song} from './Song';
-import {api} from '../api';
-import {player} from './player';
+import {LikedSongs} from './LikedSongs';
+import {SearchSongs} from './SearchSongs';
 import {useAppContext} from '../../context/AppContext';
 import {useThemedStyles} from '../theme';
 
-export function MusicPage({activeSidebarItem}) {
+export function MusicPage() {
   const styles = useThemedStyles(createStyles);
 
-  const {set} = useAppContext();
+  const {get} = useAppContext();
 
-  const [likedSongs, setLikedSongs] = useState([]);
+  const pages = {
+    search: <SearchSongs />,
+    liked: <LikedSongs />,
+  };
 
-  useEffect(() => {
-    api.get('/music').then(response => {
-      setLikedSongs(response);
-    });
-  }, []);
 
   return (
     <View style={styles.content}>
-      <NowPlayingBanner />
-
-      <Pressable onPress={() => player.playPlaylist(likedSongs, set)}>
-        <Text>Play All</Text>
-      </Pressable>
-
-      {likedSongs.map((song, index) => (
-        <Song key={song.isrc} song={song} isEven={index % 2 === 0} onClick={() => player.playPlaylist(likedSongs, set, index)} />
-      ))}
+      {pages[get("app.activeSideBarItem")]}
     </View>
   );
 }
 
 const createStyles = colors => StyleSheet.create({
-  statsContainer: {
-    flexDirection: 'row',
-    gap: 16,
-    paddingTop: 16,
-  },
-  greeting: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    marginTop: 16,
-    color: colors.outline
-  },
 });
