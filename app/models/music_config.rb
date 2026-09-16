05a2215283cc2aea5335ae6acd9d41cf5391cb32
Lucas <contact@lucasvanbriemen.nl>
2026-09-16 19:52:59 +0200

    Remove redundant code in homepage and music configuration files.

--- app/models/music_config.rb
+++ app/models/music_config.rb
@@ -2,33 +2,19 @@ module MusicConfig
   GROUPS = [
     {
       path: "home",
-      name: "Home",
-      send_notifications: true,
-      rules: { exclude_from: %w[work github pathe] }
+      name: "Home"
     },
     {
       path: "work",
-      name: "Work",
-      send_notifications: true,
-      rules: {
-        from: %w[*@webinargeek.com],
-        to: %w[*@webinargeek.com]
-      }
+      name: "Work"
     },
     {
       path: "github",
-      name: "GitHub",
-      send_notifications: false,
-      rules: {
-        from: %w[*@github.com *@notifications.github.com],
-        sender_name: [ "github GUI" ]
-      }
+      name: "GitHub"
     },
     {
       path: "pathe",
-      name: "Pathe",
-      send_notifications: true,
-      rules: { from: %w[*@service.pathe.nl] }
+      name: "Pathe"
     }
   ].freeze
 end
