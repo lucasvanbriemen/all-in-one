ba96cbb0f287d244ddb7160e9fc04e177a41e18f
Lucas <contact@lucasvanbriemen.nl>
2026-09-16 20:44:58 +0200

    Play just one song.

--- react-native/components/music/SearchSongs.jsx
+++ react-native/components/music/SearchSongs.jsx
@@ -38,7 +38,7 @@ export function SearchSongs() {
       />
 
       {searchResults.map((song, index) => (
-        <Song key={song.isrc} song={song} isEven={index % 2 === 0} onClick={() => player.playPlaylist(searchResults, set, index)} />
+        <Song key={song.isrc} song={song} isEven={index % 2 === 0} onClick={() => player.play(song, set)} />
       ))}
     </View>
   );
