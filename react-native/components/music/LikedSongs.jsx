b0cc519e9997517744ce077d47184f1fccc3b7e7
Lucas <contact@lucasvanbriemen.nl>
2026-09-17 13:48:40 +0200

    Style Songs component with improved UI design.

--- react-native/components/music/LikedSongs.jsx
+++ react-native/components/music/LikedSongs.jsx
@@ -23,13 +23,13 @@ export function LikedSongs() {
 
   return (
     <View style={styles.content}>
-      <Text style={styles.greeting}>Liked Songs</Text>
-
-      <Text style={styles.subheading}>{likedSongs.length} songs, {(likedSongs.reduce((total, song) => total + song.duration, 0) / 60).toFixed(0)} minutes</Text>
-
-      <Pressable onPress={() => player.playPlaylist(likedSongs, set)} style={styles.playAllButton}>
-        <Text>Play All</Text>
-      </Pressable>
+      <View style={styles.details}>
+        <Text style={styles.greeting}>Liked Songs</Text>
+        <Text style={styles.subheading}>{likedSongs.length} songs, {(likedSongs.reduce((total, song) => total + song.duration, 0) / 60).toFixed(0)} minutes</Text>
+        <Pressable onPress={() => player.playPlaylist(likedSongs, set)} style={styles.playAllButton}>
+          <Text style={styles.playAllButtonText}>Play All</Text>
+        </Pressable>
+      </View>
 
       {likedSongs.map((song, index) => (
         <Song key={song.isrc} song={song} isEven={index % 2 === 0} onClick={() => player.playPlaylist(likedSongs, set, index)} />
@@ -39,15 +39,30 @@ export function LikedSongs() {
 }
 
 const createStyles = colors => StyleSheet.create({
-  statsContainer: {
-    flexDirection: 'row',
-    gap: 16,
-    paddingTop: 16,
-  },
   greeting: {
-    fontSize: 32,
+    fontSize: 24,
     fontWeight: 'bold',
-    marginTop: 16,
-    color: colors.outline
+    color: colors.onSurface
+  },
+  subheading: {
+    fontSize: 16,
+    color: colors.onSurfaceVariant,
+    marginVertical: 8
+  },
+  playAllButton: {
+    backgroundColor: colors.primary,
+    padding: 12,
+    borderRadius: 100,
+    alignItems: 'center',
+    marginVertical: 16,
+ },
+  playAllButtonText: {
+    color: colors.onPrimary,
+    fontWeight: 'bold',
+  },
+  details: {
+    marginVertical: 24,
+    width: '50%',
+    marginLeft: '25%',
   }
 });
