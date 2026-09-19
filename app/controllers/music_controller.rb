67eb322ac453d49c5c10a14ff3b3bfbc2e697e88
Lucas <contact@lucasvanbriemen.nl>
2026-09-19 15:10:42 +0200

    Expose more data in API response and UI display.

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -25,10 +25,14 @@ class MusicController < ApplicationController
 
     seconds_played = Music::Play.sum(&:seconds_played)
     time_played = TimeConversion.seconds_to_human_readable(seconds_played)
+    total_plays = Music::Play.count
+    different_plays = Music::Play.select(:song_isrc).distinct.count
 
     render json: {
       top_songs: top_songs,
-      time_played: time_played
+      time_played: time_played,
+      total_plays: total_plays,
+      different_plays: different_plays
     }
   end
 
--- react-native/components/music/Statistics.jsx
+++ react-native/components/music/Statistics.jsx
@@ -27,6 +27,8 @@ export function Statistics() {
       <View style={[styles.details, isCompact && styles.detailsCompact]}>
         <Text style={styles.subheading}>{statistics?.top_songs?.length ?? 0} songs, {(statistics?.top_songs?.reduce((total, song) => total + song.duration, 0) / 60).toFixed(0)} minutes</Text>
         <Text style={styles.subheading}>{statistics?.time_played ?? 0} played, {statistics?.top_songs?.reduce((total, song) => total + song.times_played, 0) ?? 0} times played</Text>
+        <Text style={styles.subheading}>{statistics?.total_plays ?? 0} plays</Text>
+        <Text style={styles.subheading}>{statistics?.different_plays ?? 0} different plays</Text>
       </View>
 
       {statistics?.top_songs?.map((song, index) => (
