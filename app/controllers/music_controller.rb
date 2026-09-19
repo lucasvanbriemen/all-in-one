731bf62f1d64655b21e5392256921ea2306dcd41
Lucas <contact@lucasvanbriemen.nl>
2026-09-19 20:22:06 +0200

    Move to stats.

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -15,27 +15,6 @@ class MusicController < ApplicationController
     render json: Music::SongSearch.new.search(term)
   end
 
-  def stats
-    # Get the songs that have the most seconds played
-    top_songs = Music::Song.joins(:plays)
-                           .select("songs.*, SUM(plays.seconds_played) AS total_seconds_played, COUNT(plays.id) AS times_played")
-                           .group("songs.isrc")
-                           .order("total_seconds_played DESC")
-                           .limit(10)
-
-    seconds_played = Music::Play.sum(&:seconds_played)
-    time_played = TimeConversion.seconds_to_human_readable(seconds_played)
-    total_plays = Music::Play.count
-    different_plays = Music::Play.select(:song_isrc).distinct.count
-
-    render json: {
-      top_songs: top_songs,
-      time_played: time_played,
-      total_plays: total_plays,
-      different_plays: different_plays
-    }
-  end
-
   def toggle_favorite
     song = Music::Song.find(params[:id])
     song.toggle_favorite!
