f9212a8f42893c4d0855f71e11fadae950ae0f9b
Lucas <contact@lucasvanbriemen.nl>
2026-09-19 14:27:43 +0200

    Add new Statistics component to display liked songs statistics.

---
+++ react-native/components/music/Statistics.jsx
@@ -0,0 +1,73 @@
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+
+import {Song} from './Song';
+import {api} from '../api';
+import {player} from './player';
+import {useAppContext} from '../../context/AppContext';
+import {useCompactLayout} from '../useCompactLayout';
+import {useThemedStyles} from '../theme';
+
+export function Statistics() {
+  const styles = useThemedStyles(createStyles);
+  const isCompact = useCompactLayout();
+
+  const {set} = useAppContext();
+
+  const [likedSongs, setLikedSongs] = useState([]);
+
+  useEffect(() => {
+    api.get('/music').then(response => {
+      setLikedSongs(response);
+    });
+  }, []);
+
+  return (
+    <View style={styles.content}>
+      <View style={[styles.details, isCompact && styles.detailsCompact]}>
+        <Text style={styles.greeting}>Liked Songs</Text>
+        <Text style={styles.subheading}>{likedSongs.length} songs, {(likedSongs.reduce((total, song) => total + song.duration, 0) / 60).toFixed(0)} minutes</Text>
+        <Pressable onPress={() => player.playPlaylist(likedSongs, set)} style={styles.playAllButton}>
+          <Text style={styles.playAllButtonText}>Play All</Text>
+        </Pressable>
+      </View>
+
+      {likedSongs.map((song, index) => (
+        <Song key={song.isrc} song={song} isEven={index % 2 === 0} onClick={() => player.playPlaylist(likedSongs, set, index)} />
+      ))}
+    </View>
+  );
+}
+
+const createStyles = colors => StyleSheet.create({
+  greeting: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: colors.onSurface
+  },
+  subheading: {
+    fontSize: 16,
+    color: colors.onSurfaceVariant,
+    marginVertical: 8
+  },
+  playAllButton: {
+    backgroundColor: colors.primary,
+    padding: 12,
+    borderRadius: 100,
+    alignItems: 'center',
+    marginVertical: 16,
+ },
+  playAllButtonText: {
+    color: colors.onPrimary,
+    fontWeight: 'bold',
+  },
+  details: {
+    marginVertical: 24,
+    width: '50%',
+    marginLeft: '25%',
+  },
+  detailsCompact: {
+    width: '100%',
+    marginLeft: '0%',
+  }
+});
